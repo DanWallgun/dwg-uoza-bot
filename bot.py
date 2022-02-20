456a7bd5667419481474d51cae1ae0d5d6a3145b
DanWallgun <dnl.drdvch@gmail.com>
2022-02-20 20:45:02 +0300

    Initial

---
+++ bot.py
@@ -0,0 +1,43 @@
+import logging
+import os
+import random
+import uuid
+
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage, AnswerInlineQuery
+
+
+async def echo(message: types.Message):
+    return SendMessage(message.chat.id, message.as_json())
+
+
+async def inline_random_case(inline_query: types.InlineQuery):
+    transform = lambda text: "".join(
+        [
+            character.upper() if random.getrandbits(1) else character
+            for character in text.lower()
+        ]
+    )
+    transformed_text = transform(inline_query.query)
+    return AnswerInlineQuery(
+        inline_query_id=inline_query.id,
+        results=[
+            types.InlineQueryResultArticle(
+                id=str(uuid.uuid4()),
+                title="Random case",
+                description=transformed_text,
+                input_message_content=types.InputTextMessageContent(transformed_text),
+            )
+        ],
+    )
+
+
+def prepare_dispatcher() -> Dispatcher:
+    logging.getLogger().setLevel(logging.INFO)
+    dispatcher = Dispatcher(Bot(os.getenv("TELEGRAM_BOT_TOKEN")))
+    dispatcher.setup_middleware(LoggingMiddleware())
+    dispatcher.register_message_handler(echo)
+    dispatcher.register_inline_handler(inline_random_case)
+    return dispatcher
---
+++ ycf.py
@@ -0,0 +1,24 @@
+import json
+
+from aiogram import types
+from aiogram.dispatcher.webhook import BaseResponse
+
+from bot import prepare_dispatcher
+
+
+async def handler(event, context):
+    if event["httpMethod"] != "POST":
+        return {"statusCode": 400}
+
+    dispatcher = prepare_dispatcher()
+    results = await dispatcher.process_update(
+        types.Update.to_object(json.loads(event["body"]))
+    )
+    for result in results:
+        if isinstance(result, BaseResponse):
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps(result.get_response()),
+            }
+    return {"statusCode": 200}
